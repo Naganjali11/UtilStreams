064ddf703d122587c4980b701261a4bd75438721
Naganjali11 <naganjali425@gmail.com>
2025-10-31 11:36:51 +0530

    javafullstackprograms

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.codegnan.utilstreams;
+
+public class Employee {
+int id;
+String name;
+double salary;
+String department;
+public Employee(int id, String name, double salary, String department) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.salary = salary;
+	this.department = department;
+}
+@Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", department=" + department + "]";
+}
+
+
+}
